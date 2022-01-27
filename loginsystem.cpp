ebb8372c276914d1d4731140847b42498bf4b744
elerma12 <70728294+elerma12@users.noreply.github.com>
2022-01-27 15:59:32 -0600

    Update loginsystem.cpp

--- loginsystem.cpp
+++ loginsystem.cpp
@@ -15,4 +15,57 @@ bool IsLoggedIn ()
  
  
  
-  ifstream read ("data \\ + username + " txt')
+  ifstream read ("c:\\ + username + " txt');
+  getline( read, un);
+  getline( read, pw);
+  if ( un ==username && pw == password) 
+  {
+    return true;
+  }
+  else
+  {
+    return false;
+  }
+               
+int main()
+{
+		int choice; 
+		
+		cout << ": Register/2: Login\nYour choice:" ; cin >> choice;
+		if ( choice == 1)
+		{
+			string username,password;
+			
+			cout << "Select a username: " cin << username;
+			cout << "Select a password: " cin << password;
+			
+			ofstream file; 
+			file("c:\\" + username+ ".txt");
+			
+			file << username << endl << password; 
+			file.close();
+			
+			main ();
+		}
+	
+	else if (choice ==2)
+	{
+		bool status = IsLoggedIn();
+		
+		if (!status)
+		{
+			cout << " False Login! << endl;
+			system ("PAUSE");
+			return 0;
+		}
+		else
+		{
+			cout << " Successfully logged in! " << endl;
+			system("PAUSE");
+			return 1
+		}
+}
+                 
+
+                  
+   
