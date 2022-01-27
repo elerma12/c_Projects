87f91df44e9bff449f9572e389d1bcec865b0911
elerma12 <70728294+elerma12@users.noreply.github.com>
2022-01-27 16:05:01 -0600

    Update loginsystem.cpp

--- loginsystem.cpp
+++ loginsystem.cpp
@@ -6,32 +6,31 @@ using namespace std;
 
 bool IsLoggedIn ()
 {
-  string password , username, un, pw;
-  cout<< " Enter Username: " >> ; 
-  cin >> username;
-  cout << " Enter Password:  " ;
-  cin >> password ;
- }
+   string password , username, un, pw;
+   cout<< " Enter Username: " >> ; 
+   cin >> username;
+   cout << " Enter Password:  " ;
+   cin >> password ;
  
- 
- 
-  ifstream read ("c:\\ + username + " txt');
-  getline( read, un);
-  getline( read, pw);
-  if ( un ==username && pw == password) 
-  {
-    return true;
-  }
-  else
-  {
-    return false;
-  }
-               
+   ifstream read ("c:\\ + username + " txt');
+   getline( read, un);
+   getline( read, pw);
+   if ( un ==username && pw == password) 
+   {
+     return true;
+   }
+   else
+   {
+     return false;
+   }
+		 		 
+}             
 int main()
 {
    int choice; 
 		
-   cout << ": Register/2: Login\nYour choice:" ; cin >> choice;
+   cout << "1: Register/n2: Login\nYour choice:" ; cin >> choice;
+	
    if ( choice == 1)
    {
       string username,password;
