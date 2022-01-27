25c5835396bca1d7445773f1c2cfc60dc6d10ddb
elerma12 <70728294+elerma12@users.noreply.github.com>
2022-01-27 08:05:16 -0600

    Create loginsystem.cpp

---
+++ loginsystem.cpp
@@ -0,0 +1,18 @@
+# include < iostream > 
+# include < fstream > 
+# include <string>
+
+using namespace std;
+
+bool IsLoggedIn ()
+{
+  string password , username, un, pw;
+  cout<< " Enter Username: " >> ; 
+  cin >> username;
+  cout << " Enter Password:  " ;
+  cin >> password ;
+ }
+ 
+ 
+ 
+  ifstream read ("data \\ + username + " txt')
